1828343ab9caa453b32e38bab15e3e801771be59
LEEJS-32 <jianshenglee32@gmail.com>
2025-02-20 00:54:35 +0800

    rename testing1.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php include '_header.php' ?>
 <?php require 'database.php' ?>
 
-<a href="pages/testing1.php">Sign Up/Login</a>
+<a href="pages/signup_login.php">Sign Up/Login</a>
 <a href="pages/add_to_cart.php">Add to cart</a>
 <a href="pages/view_cart.php">View cart</a>
 
