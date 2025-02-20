c44f4dcd125b1cd00de1d42c8fc5cc2cc8d56f48
LEEJS-32 <jianshenglee32@gmail.com>
2025-02-20 17:23:45 +0800

    some designs on home and adjustment on footer

---
+++ pages/home.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible"
+        content="IE=edge">
+        <meta name="viewport"
+        content="width=device-width, initial-scale=1.0">
+        <!-- <title>Header</title> -->
+        <link rel="stylesheet" href="../css/style.css">
+        <link rel="stylesheet" href="../css/logo.css">
+        <link rel="stylesheet" href="../css/footer.css">
+        <link rel="stylesheet" href="../css/home.css">
+        <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200..1000;1,200..1000&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+    </head>
+
+    <body>
+        <?php include '../_header.php'; ?>
+        
+        <main class="main">
+        <div class="intro">
+            <div class="intro-left">
+                <h3>#The Best Online Furniture Store</h3>
+                
+                <div class='intro-big-content'>
+                <h2><pre>Your Home, Your Style</pre></h2>
+
+                <h1><pre>Our Passion</pre></h1>
+                </div>
+                
+                <p class="intro-des">Crafting stylish and comfortable furniture with passion, making your house feel like home.</p>
+                <div class="shop-now">
+                    <button class="shop-now-btn">Shop Now&nbsp;<i class='bx bx-right-arrow-alt'></i></button>
+                    <a href="" class="view-cat-link">View All Categories</a>
+                </div>
+            </div>
+
+            <div class="intro-right">
+                <img src="../img/intro.jpeg" class="img-intro">
+            </div>
+        </main>
+
+        <?php include '../_footer.php'; ?>
+    </body>
+
+</html>
