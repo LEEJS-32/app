1f4645c15d093fb5908e0fc681b33021b940df43
LEEJS-32 <jianshenglee32@gmail.com>
2025-03-16 10:00:25 +0800

    bug fix on auth_user

--- _header.php
+++ _header.php
@@ -9,6 +9,7 @@
         <!-- <title>Header</title> -->
         <link rel="stylesheet" href="\css\header.css">
         <link rel="stylesheet" href="\css\logo.css">
+        <link rel="stylesheet" href="\css\style.css">
         <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
